fe3d2f2269bdee75f8709888342c801bdc1ce88e
ysl72021yagi <shunta.yagi@ysl.co.jp>
2021-07-12 16:20:24 +0900

    yagi

--- systemofj/src/model/User.java
+++ systemofj/src/model/User.java
@@ -2,4 +2,48 @@ package model;
 
 public class User {
 
+	private int u_id;
+	private String u_name;
+	private String u_first;
+	private String u_pw;
+	private int a_flag;
+	private int y_flag;
+
+
+	public int getU_id() {
+		return u_id;
+	}
+	public void setU_id(int u_id) {
+		this.u_id = u_id;
+	}
+	public String getU_name() {
+		return u_name;
+	}
+	public void setU_name(String u_name) {
+		this.u_name = u_name;
+	}
+	public String getU_first() {
+		return u_first;
+	}
+	public void setU_first(String u_first) {
+		this.u_first = u_first;
+	}
+	public String getU_pw() {
+		return u_pw;
+	}
+	public void setU_pw(String u_pw) {
+		this.u_pw = u_pw;
+	}
+	public int getA_flag() {
+		return a_flag;
+	}
+	public void setA_flag(int a_flag) {
+		this.a_flag = a_flag;
+	}
+	public int getY_flag() {
+		return y_flag;
+	}
+	public void setY_flag(int y_flag) {
+		this.y_flag = y_flag;
+	}
 }
