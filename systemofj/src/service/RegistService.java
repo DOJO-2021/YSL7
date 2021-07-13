d34be5cb7464cb8c1c36e81302fc84aebf3a9d86
yuri-kageyama <yuri.kageyama@ysl.co.jp>
2021-07-13 16:30:27 +0900

    RegistService 更新

--- systemofj/src/service/RegistService.java
+++ systemofj/src/service/RegistService.java
@@ -8,25 +8,25 @@ import dao.UserDao;
 
 public class RegistService {
 
-	public int studentInsert(String sName, String sKana, String sContent, String sUnivercity, String sFaculty, String sDepartment, String sAddress, String sPcmail, String sMobilemail, String sCareertasu, String sMynavi, String sRikunavi, String sOther, int alleditflag)  throws ClassNotFoundException, SQLException {
-		int result = 0;
+	public boolean studentInsert(String sName, String sKana, String sContent, String sUnivercity, String sFaculty, String sDepartment, String sAddress, String sPcmail, String sMobilemail, String sCareertasu, String sMynavi, String sRikunavi, String sOther, int alleditflag)  throws ClassNotFoundException, SQLException {
+		boolean result = false;
 
 		//ドライバの登録
 		Class.forName("org.h2.Driver");
 
 		//データベースに接続する
-		Connection conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/ShareNote", "sa", "sa");
+		Connection conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj","sa","sa");
 		//Daoを実体化
 		UserDao dao = new UserDao(conn);
 
 		//引数を渡し、取得値をbeanに渡す
-		User newStudent = dao.studentInsert(sName, sKana, sContent, sUnivercity, sFaculty, sDepartment, sAddress, sPcmail, sMobilemail, sCareertasu, sMynavi, sRikunavi, sOther, alleditflag);
+		int newStudent = dao.studentInsert(sName, sKana, sContent, sUnivercity, sFaculty, sDepartment, sAddress, sPcmail, sMobilemail, sCareertasu, sMynavi, sRikunavi, sOther, alleditflag);
 
-
+		if(newStudent != 0) {
+			result = true;
 		}
 
-
-	return newStudent;
+		return result;
 	}
 
 }
