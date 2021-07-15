49b637119f73db90772f7e60f87fb80ee01a3234
yumiysl7 <yumi.saito@ysl.co.jp>
2021-07-15 17:19:41 +0900

    aaa

--- systemofj/src/dao/TemplateDao.java
+++ systemofj/src/dao/TemplateDao.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import model.Template;
 
@@ -34,7 +35,7 @@ public class TemplateDao {
 	}
 
 	//更新
-	public int eventUpdate(int tId, String tTitle, String tContent) throws SQLException {
+	public int templateUpdate(int tId, String tTitle, String tContent) throws SQLException {
 
 		//SQL文を準備する
 		String sql = "update Template set t_title=?, t_content=? where t_id=?";
@@ -84,7 +85,130 @@ public class TemplateDao {
 	}
 
 	//インターンSELECT
-	public Template
+	public ArrayList<Template> templateInternTitleSelect() throws SQLException {
+
+		ArrayList<Template> templateInternList = new ArrayList<Template>();
+
+		//SQL文を準備する
+		String sql = "select * from template where t_category='インターン'";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//SQLを実行し、結果を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		while (rs.next()) {
+			Template templateIntern = new Template();
+			templateIntern.settId(rs.getInt("t_id"));
+			templateIntern.settTitle(rs.getString("t_title"));
+			templateIntern.settContent(rs.getString("t_content"));
+			templateIntern.settCategory(rs.getString("t_category"));
+
+			templateInternList.add(templateIntern);
+
+		}
+
+		if(conn != null) {
+			conn.close();
+		}
+
+		return templateInternList;
+
+	}
+
+	//セミナーSELECT
+	public ArrayList<Template> templateSeminarTitleSelect() throws SQLException {
+
+		ArrayList<Template> templateSeminarList = new ArrayList<Template>();
+
+		//SQL文を準備する
+		String sql = "select * from template where t_category='説明会'";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//SQLを実行し、結果を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		while (rs.next()) {
+			Template templateSeminar = new Template();
+			templateSeminar.settId(rs.getInt("t_id"));
+			templateSeminar.settTitle(rs.getString("t_title"));
+			templateSeminar.settContent(rs.getString("t_content"));
+			templateSeminar.settCategory(rs.getString("t_category"));
+
+			templateSeminarList.add(templateSeminar);
+
+		}
+
+		if(conn != null) {
+			conn.close();
+		}
+
+		return templateSeminarList;
+
+	}
+
+	//面接SELECT
+	public ArrayList<Template> templateFaceTitleSelect() throws SQLException {
+
+		ArrayList<Template> templateFaceList = new ArrayList<Template>();
+
+		//SQL文を準備する
+		String sql = "select * from template where t_category='説明会'";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//SQLを実行し、結果を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		while (rs.next()) {
+			Template templateFace = new Template();
+			templateFace.settId(rs.getInt("t_id"));
+			templateFace.settTitle(rs.getString("t_title"));
+			templateFace.settContent(rs.getString("t_content"));
+			templateFace.settCategory(rs.getString("t_category"));
+
+			templateFaceList.add(templateFace);
+
+		}
+
+		if(conn != null) {
+			conn.close();
+		}
+
+		return templateFaceList;
+
+	}
+
+	//その他SELECT
+	public ArrayList<Template> templateOtherTitleSelect() throws SQLException {
+
+		ArrayList<Template> templateOtherList = new ArrayList<Template>();
+
+		//SQL文を準備する
+		String sql = "select * from template where t_category='その他'";
+		PreparedStatement pStmt = conn.prepareStatement(sql);
+
+		//SQLを実行し、結果を取得する
+		ResultSet rs = pStmt.executeQuery();
+
+		while (rs.next()) {
+			Template templateOther = new Template();
+			templateOther.settId(rs.getInt("t_id"));
+			templateOther.settTitle(rs.getString("t_title"));
+			templateOther.settContent(rs.getString("t_content"));
+			templateOther.settCategory(rs.getString("t_category"));
+
+			templateOtherList.add(templateOther);
+
+		}
+
+		if(conn != null) {
+			conn.close();
+		}
+
+		return templateOtherList;
+
+	}
+
+
 
 
 }
