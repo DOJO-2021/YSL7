6a2dc4f9dbe1569e73468c1f4148f39e2d01b111
tsujiayano <ayano.tsuji@ysl.co.jp>
2021-07-20 17:16:52 +0900

    tsuji

--- systemofj/src/test/EventDaoTest.java
+++ systemofj/src/test/EventDaoTest.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import dao.EventDao;
+import dao.StudentDao;
 import model.SEvent;
 
 public class EventDaoTest {
@@ -21,9 +22,9 @@ public class EventDaoTest {
 		//データベースへの接続情報を設定する
 		Connection conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
 
-		//idあっている人のデータ持ってこれるかテスト
+		//選択したIDの人のデータ持ってこれるかテスト
 		EventDao eventdao = new EventDao(conn);
-		System.out.println("idあっている人のデータ持ってこれるかテスト----------------------");
+		System.out.println("選択したIDの人のデータ持ってこれるかテスト----------------------");
 		ArrayList<SEvent> sEventList = eventdao.eventSelect(1);
 		if(sEventList != null) {
 			System.out.println("一応成功");
@@ -38,30 +39,40 @@ public class EventDaoTest {
 		}
 
 		//登録テスト
-//		System.out.println("登録テスト----------------------");
-//		conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
-//		eventdao = new EventDao(conn);
-//		conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
-//		StudentDao studentdao = new StudentDao(conn);
-//		try {
-//			int s = studentdao.studentInsert("辻彩乃","ツジアヤノ","文系","産業能率大学","経営学部","現代ビジネス学科","神奈川県","ayano@ee","ayano@ee","","","","");
-//			int t = eventdao.eventInsert("座談会１","2020-10-18");
-//			System.out.println("成功");
-//		}catch(SQLException e){
-//			System.out.println("失敗");
-//			e.printStackTrace();
-//		}
+		System.out.println("登録テスト----------------------");
+		conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
+		eventdao = new EventDao(conn);
+		conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
+		StudentDao studentdao = new StudentDao(conn);
+		try {
+			int s = studentdao.studentInsert("辻彩乃","ツジアヤノ","文系","産業能率大学","経営学部","現代ビジネス学科","神奈川県","ayano@ee","ayano@ee","","","","");
+			int t = eventdao.eventInsert("座談会１","2020-10-18");
+			System.out.println("成功");
+		}catch(SQLException e){
+			System.out.println("失敗");
+			e.printStackTrace();
+		}
 
 		//更新テスト
 		System.out.println("更新テスト----------------------");
 		conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
 		eventdao = new EventDao(conn);
-			int up = eventdao.eventUpdate(9,"2020-10-18");
+			int up = eventdao.eventUpdate(9,"2020-10-20");
 			if(up>0) {
 				System.out.println("成功！");
 			}else{
 				System.out.println("失敗！");
 		}
 
+		//削除テスト
+			System.out.println("削除テスト----------------------");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:\\pleiades\\workspace\\YSL7\\data\\systemofj", "sa", "sa");
+			eventdao = new EventDao(conn);
+			int de = eventdao.eventDelete(9);
+			if(up>0) {
+				System.out.println("成功！");
+			}else{
+				System.out.println("失敗！");
+		}
 	}
 }
